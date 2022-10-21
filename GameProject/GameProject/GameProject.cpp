6971d8d9c6b93d9c6a47b208f73478cd0e183755
leeinbeom <dqdq7979@naver.com>
2022-10-22 01:54:35 +0900

    Game Proejct Start

---
+++ GameProject/GameProject/GameProject.cpp
@@ -0,0 +1,301 @@
+﻿// GameProject.cpp : 애플리케이션에 대한 진입점을 정의합니다.
+//
+
+#include "framework.h"
+#include "GameProject.h"
+
+
+#define MAX_LOADSTRING 100
+
+// 전역 변수:
+HINSTANCE hInst;                                // 현재 인스턴스입니다.
+WCHAR szTitle[MAX_LOADSTRING];                  // 제목 표시줄 텍스트입니다.
+WCHAR szWindowClass[MAX_LOADSTRING];            // 기본 창 클래스 이름입니다.
+
+// 이 코드 모듈에 포함된 함수의 선언을 전달합니다:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: 여기에 코드를 입력합니다.
+
+    // 전역 문자열을 초기화합니다.
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_GAMEPROJECT, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // 애플리케이션 초기화를 수행합니다:
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_GAMEPROJECT));
+
+    MSG msg;
+
+    // 기본 메시지 루프입니다:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int) msg.wParam;
+}
+
+
+
+//
+//  함수: MyRegisterClass()
+//
+//  용도: 창 클래스를 등록합니다.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_GAMEPROJECT));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_GAMEPROJECT);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+//
+//   함수: InitInstance(HINSTANCE, int)
+//
+//   용도: 인스턴스 핸들을 저장하고 주 창을 만듭니다.
+//
+//   주석:
+//
+//        이 함수를 통해 인스턴스 핸들을 전역 변수에 저장하고
+//        주 프로그램 창을 만든 다음 표시합니다.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
+
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, 900, 600, nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+//
+//  함수: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  용도: 주 창의 메시지를 처리합니다.
+//
+//  WM_COMMAND  - 애플리케이션 메뉴를 처리합니다.
+//  WM_PAINT    - 주 창을 그립니다.
+//  WM_DESTROY  - 종료 메시지를 게시하고 반환합니다.
+//
+//
+
+
+
+RECT GameLine;
+RECT AbilityLine;
+RECT ME;
+
+
+//ME 스텟
+int HP;
+int Power;
+int Speed;
+int SumExp;
+int Exp;
+
+//ME 스텟 텍스트
+wchar_t HPstr[2];
+wchar_t Powerstr[2];
+wchar_t Speedstr[2];
+wchar_t SumExpstr[2];
+wchar_t Expstr[2];
+
+
+
+
+
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_COMMAND:
+        {
+            int wmId = LOWORD(wParam);
+            // 메뉴 선택을 구문 분석합니다:
+            switch (wmId)
+            {
+            case IDM_ABOUT:
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+                break;
+            case IDM_EXIT:
+                DestroyWindow(hWnd);
+                break;
+            default:
+                return DefWindowProc(hWnd, message, wParam, lParam);
+            }
+        }
+        break;
+    case WM_CREATE:
+    {
+        //GameLine 크기 설정
+        GameLine.left = 20;
+        GameLine.top = 20;
+        GameLine.right = 520;
+        GameLine.bottom = 520;
+
+        //AbilityLine 크기 설정
+        AbilityLine.left = 550;
+        AbilityLine.top = 20;
+        AbilityLine.right = 800;
+        AbilityLine.bottom = 520;
+
+        //ME 크기 설정
+        ME.left = 260;
+        ME.top = 260;
+        ME.right = 280;
+        ME.bottom = 280;
+
+        //ME 기본스텟 설정
+        HP = 5;
+        Power = 1;
+        Speed = 5;
+        SumExp = 5;
+        Exp = 0;
+        
+
+        //ME 기본스텟 텍스트 설정
+        wsprintfW(HPstr, L"%d", HP);
+        wsprintfW(Powerstr, L"%d", Power);
+        wsprintfW(Speedstr, L"%d", Speed);
+        wsprintfW(Expstr, L"%d / %d", Exp,SumExp);
+
+    }
+        break;
+    case WM_KEYDOWN:
+    {
+        switch (wParam)
+        {
+        case VK_LEFT:
+            ME.left -= Speed;
+            ME.right -= Speed;
+            break;
+        case VK_RIGHT:
+            ME.left += Speed;
+            ME.right += Speed;
+            break;
+        case VK_UP:
+            ME.top -= Speed;
+            ME.bottom -= Speed;
+            break;
+        case VK_DOWN:
+            ME.top += Speed;
+            ME.bottom += Speed;
+            break;
+        }
+
+        InvalidateRect(hWnd, NULL, true);
+    }
+        break;
+    case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hWnd, &ps);
+            // TODO: 여기에 hdc를 사용하는 그리기 코드를 추가합니다...
+
+            //GameLine 선 그리기
+            Rectangle(hdc, GameLine.left, GameLine.top, GameLine.right, GameLine.bottom);
+
+            //AbilityLine 선 그리기
+            Rectangle(hdc, AbilityLine.left, AbilityLine.top, AbilityLine.right, AbilityLine.bottom);
+
+            //Ability 텍스트 출력
+            TextOut(hdc, 570, 50, L"체력", 2);
+            TextOut(hdc, 570, 80, L"데미지", 3);
+            TextOut(hdc, 570, 110, L"이동속도", 4);
+            TextOut(hdc, 570, 140, L"경험치", 3);
+
+
+            //Ability
+            TextOut(hdc, 770, 50, HPstr, lstrlen(HPstr));
+            TextOut(hdc, 770, 80, Powerstr, lstrlen(Powerstr));
+            TextOut(hdc, 770, 110, Speedstr, lstrlen(Speedstr));
+            TextOut(hdc, 770, 140, Expstr, lstrlen(Expstr));
+
+
+            //ME 선 그리기
+            Rectangle(hdc, ME.left, ME.top, ME.right, ME.bottom);
+
+
+
+
+
+            
+
+
+
+            EndPaint(hWnd, &ps);
+        }
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// 정보 대화 상자의 메시지 처리기입니다.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
